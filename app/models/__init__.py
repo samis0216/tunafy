f64863578b6d7d2595897eb9def5d2b5abec3e11
Lyndon <tzlyndon@gmail.com>
2024-01-19 13:05:42 -0800

    create tables

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
 from .db import db
 from .user import User
+from .song import Song
+from .album import Album
+from .playlist import Playlist
+from .song_like import SongLike
 from .db import environment, SCHEMA
