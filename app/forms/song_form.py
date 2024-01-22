560357d5e2680dad3c9c3f8c7b4d8c0e65b523ce
victoria <victorian114@gmail.com>
2024-01-22 10:35:47 -0800

    file validators added

--- app/forms/song_form.py
+++ app/forms/song_form.py
@@ -7,8 +7,8 @@ from app.models import Song
 class SongForm(FlaskForm):
     song_name = StringField('Song Name', validators=[DataRequired()])
     album_name = StringField('Album Name', validators=[DataRequired()])
-    song_cover_url = StringField('Song Cover URL', validators=[DataRequired()])
-    song_file_url = StringField('Song File URL', validators=[DataRequired()])
+    song_cover_url = FileField('Song Cover URL', validators=[DataRequired(), FileAllowed(), FileRequired()])
+    song_file_url = FileField('Song File URL', validators=[DataRequired(), FileAllowed(), FileRequired()])
     duration = IntegerField('Duration', validators=[DataRequired()])
     submit = SubmitField('Add Song')
 
