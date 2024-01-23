a72ea5a6209a2ded05763ea5223fdd185d8a2f74
Sami <samisamman26@yahoo.com>
2024-01-23 13:34:36 -0800

    more tweaks to song create page

--- react-vite/src/components/CreateSong/CreateSong.jsx
+++ react-vite/src/components/CreateSong/CreateSong.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom"
+import { redirect, useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react"
 import './CreateSong.css'
 import { useDispatch, useSelector } from "react-redux"
@@ -8,28 +8,24 @@ export default function CreateSong() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const [songName, setSongName] = useState('')
-    const [artistId, setArtistId] = useState('')
     const [song_cover, setSongCover] = useState('')
     const [song_file, setSongFile] = useState('')
     const [awsLoading, setAwsLoading] = useState(false)
     const user = useSelector(state => state.session.user)
-
-    useEffect (()=> {
-        console.log()
-    })
-
+    const artistId = user.id
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const formData = new FormData();
+        formData.append("song_name", songName)
+        formData.append("artist_id", artistId)
         formData.append("song_cover", song_cover);
-        console.log(formData)
-        formData.append("song_file", song_file)
-        console.log(formData)
+        formData.append("song_file", song_file);
         // aws uploads can be a bit slow—displaying
         // some sort of loading message is a good idea
         setAwsLoading(true);
         // await dispatch(createPost(formData));
+        navigate('/')
     }
 
     return (
@@ -44,7 +40,7 @@ export default function CreateSong() {
                     <h4>Song Name</h4>
                     <input
                         type="text"
-                        placeholder=" Song Name"
+                        placeholder="Song Name"
                         value={songName}
                         onChange={(e) => setSongName(e.target.value)}
                         className="song-inputs"
@@ -55,8 +51,11 @@ export default function CreateSong() {
                     <input
                         type="file"
                         accept="image/*"
-                        value={song_cover}
-                        onChange={(e) => setSongCover(e.target.files[0])}
+                        onChange={(e) => {
+                            setSongCover(e.target.files[0])
+                            console.log(e.target.files[0])
+                            }
+                        }
                     />
                 </div>
                 <div className="entry-container">
@@ -64,14 +63,13 @@ export default function CreateSong() {
                     <input
                         type="file"
                         accept="audio/*"
-                        value={song_file}
                         onChange={(e) => setSongFile(e.target.files[0])}
                     />
                 </div>
                 <div>
                     <button type="submit" id="submit_butt">Create song</button>
                 </div>
-                {(awsLoading) && <p>Loading...</p>}
+                {(awsLoading) && <p style={{alignSelf: "center"}}>Loading...</p>}
             </form>
         </div>
     )
