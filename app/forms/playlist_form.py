40a3d9dd075534439a4c21c584eb6acb25b70fff
Sami <samisamman26@yahoo.com>
2024-01-22 16:47:07 -0800

    finished full aws upload functionality

--- app/forms/playlist_form.py
+++ app/forms/playlist_form.py
@@ -2,9 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
+from app.api.aws_images import IMAGES_ALLOWED_EXTENSIONS
 
 class PlaylistForm(FlaskForm):
     playlist_name = StringField("Playlist Name", validators=[DataRequired()])
-    playlist_cover_url = FileField("Playlist Cover URL", validators=[DataRequired(), FileAllowed(), FileRequired()])
+    playlist_cover_url = FileField("Playlist Cover URL", validators=[FileAllowed(list(IMAGES_ALLOWED_EXTENSIONS)), FileRequired()])
     private= BooleanField("Private")
     submit = SubmitField("Add Playlist")
