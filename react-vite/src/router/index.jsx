e9b8562b9d77b394dec39cff6714474a1edf8d3e
victoria <victorian114@gmail.com>
2024-01-22 16:55:19 -0800

    home page stuff

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
+import LoadHomePage from '../components/HomePage/HomePage';
 import Layout from './Layout';
 
 export const router = createBrowserRouter([
@@ -9,7 +10,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Welcome!</h1>,
+        element: <LoadHomePage />,
       },
       {
         path: "login",
