a91dea9d67d8f022667006635ae1940b2dedccd8
Sami <samisamman26@yahoo.com>
2024-01-23 15:03:54 -0800

    added more song routes

--- app/api/album_routes.py
+++ app/api/album_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect
 from app.models import Album, db
 from app.forms.album_form import AlbumForm
-from .aws_images import upload_img_to_s3, get_unique_filename_img
+from .aws_images import upload_img_to_s3, get_unique_filename_img, remove_img_from_s3
 
 album_routes = Blueprint('album', __name__)
 
@@ -23,8 +23,21 @@ def albumSub():
         form.album_cover_url.data.filename = get_unique_filename_img(form.album_cover_url.data.filename)
         newAlbum = Album(album_name=data['album_name'],
                         artist_id=1,
-                        album_cover_url=upload_img_to_s3(form.album_cover_url.data))
+                        album_cover_url=upload_img_to_s3(form.album_cover_url.data).get('url'))
         db.session.add(newAlbum)
         db.session.commit()
-        return redirect("/api/albums")
+        return newAlbum.to_dict()
     return "Get shit on"
+
+@album_routes.route('/<int:id>')
+def singleAlbum(id):
+    album = Album.query.get(id)
+    return album.to_dict()
+
+@album_routes.route('/<int:id>', methods=["DELETE"])
+def albumDel(id):
+    album = Album.query.get(id)
+    remove_img_from_s3(album['album_cover_url'])
+    db.session.delete(album)
+    db.session.commit()
+    return "Successfully Deleted"
