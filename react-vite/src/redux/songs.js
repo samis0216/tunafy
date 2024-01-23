1c2091b649a1ea53c0c9b5178468f1d3713893a2
Lyndon <tzlyndon@gmail.com>
2024-01-23 08:14:13 -0800

    songs reducer skeleton

---
+++ react-vite/src/redux/songs.js
@@ -0,0 +1,117 @@
+const LOAD_SONGS = 'song/loadSongs';
+const ADD_SONG = 'song/addSong';
+const EDIT_SONG = 'song/editSong';
+const DELETE_SONG = 'song/deleteSong';
+
+// ACTION CREATORS
+const loadSongs = (songs) => {
+    return {
+        type: LOAD_SONGS,
+        songs
+    }
+}
+
+const addSong = (song) => {
+    return {
+        type: ADD_SONG,
+        song
+    }
+}
+
+const editSong = (songId) => {
+    return {
+        type: EDIT_SONG,
+        songId
+    }
+}
+
+const deleteSong = (songId) => {
+    return {
+        type: DELETE_SONG,
+        songId
+    }
+}
+
+// THUNK ACTION CREATORS
+
+export const loadSongsThunk = () => async(dispatch) => {
+    const res = await fetch('/api/songs')
+
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(loadSongs(data))
+        return data
+    }
+
+}
+
+export const addSongThunk = (song) => async(dispatch) => {
+    const res = await fetch('/api/songs', {
+        method: "POST",
+        headers: {"Content-Type": 'application/json'},
+        body: JSON.stringify(song)
+    })
+
+    if (res.ok) {
+        const data = await res.json()
+        dispatch(addSong(data))
+        return data
+    }
+
+}
+
+export const editSongThunk = (song, songId) => async(dispatch) => {
+    const res = await fetch(`/api/songs/${songId}`, {
+        method: "PUT",
+        headers: {"Content-Type": 'application/json'},
+        body: JSON.stringify(song)
+    })
+
+    if (res.ok) {
+        const data = await res.json()
+        dispatch(editSong(data))
+        return data
+    }
+}
+
+export const deleteSongThunk = (songId) => async(dispatch) => {
+    const res = await fetch(`/api/songs/${songId}`, {
+        method: "DELETE"
+    })
+
+    if (res.ok) {
+        dispatch(deleteSong(songId))
+    }
+
+}
+
+const initialState = {}
+
+const songReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOAD_SONGS: {
+            const newState = { ...state };
+
+            return newState;
+        }
+        case ADD_SONG: {
+            const newState = { ...state }
+
+            return newState;
+        }
+        case EDIT_SONG: {
+            const newState = { ...state }
+
+            return newState;
+        }
+        case DELETE_SONG: {
+            const newState = { ...state };
+
+            return newState;
+        }
+        default:
+            return state
+    }
+}
+
+export default songReducer
