0c896cbf4d678774bf8b9ca4a4dfbb2210d2091b
Lyndon <tzlyndon@gmail.com>
2024-01-23 12:36:53 -0800

    Album tings

--- react-vite/src/components/CreateAlbum/CreateAlbum.jsx
+++ react-vite/src/components/CreateAlbum/CreateAlbum.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react"
+import { useDispatch } from "react-redux";
 import "./CreateAlbum.css"
 
 export default function CreateAlbum() {
+  const dispatch = useDispatch();
   const [image, setImage] = useState(null);
+  const [name, setName] = useState("")
   const [imageLoading, setImageLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append("image", image);
+    formData.append("name", name)
 
     // aws uploads can be a bit slow—displaying
     // some sort of loading message is a good idea
@@ -22,11 +26,22 @@ export default function CreateAlbum() {
       onSubmit={handleSubmit}
       encType="multipart/form-data"
     >
+      <div>
+        <p>Album Name</p>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+
+      <div>
+      <p>Upload Cover Art</p>
       <input
         type="file"
         accept="image/*"
         onChange={(e) => setImage(e.target.files[0])}
       />
+      </div>
 
       <button
         type="submit"
