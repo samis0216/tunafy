cdde945ea4da295bd1d2ed109e51f7bacaf2e062
Lyndon <tzlyndon@gmail.com>
2024-01-24 17:25:01 -0800

    did some things

--- react-vite/src/components/AlbumDetails/AlbumDetails.jsx
+++ react-vite/src/components/AlbumDetails/AlbumDetails.jsx
@@ -1,19 +1,32 @@
-import { useState } from "react"
-import { useSelector } from "react-redux"
+import { useState, useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
+import { loadOneAlbumThunk } from "../../redux/albums"
 import './AlbumDetails.css'
-export default function AlbumDetails({album}) {
-  const albums = useSelector((store) => store.albums)
+
+export default function AlbumDetails() {
+  const dispatch = useDispatch()
   const {albumId} = useParams()
-  console.log(albumId)
+
+  useEffect(() => {
+    dispatch(loadOneAlbumThunk(albumId))
+
+  }, [dispatch, albumId])
+
+  const albumObj = useSelector((store) => store.albums)
+  const album = albumObj[albumId]
+  console.log(album)
+  if (!album) {
+    return null
+  }
   return (
     <div className="album-details-container">
       <div className="album-image-container">
-        {/* <img src={album.album_cover_url} alt="album-details-cover" /> */}
+        <img src={album.album_cover_url} alt="album-details-cover" />
       </div>
-      <div>
+      <div className="album-headers">
         <p>Album</p>
-        {/* <h1>{album.album_name}</h1> */}
+        <h1>{album.album_name}</h1>
         <p>Artist ‧  ‧ duration</p>
       </div>
     </div>
