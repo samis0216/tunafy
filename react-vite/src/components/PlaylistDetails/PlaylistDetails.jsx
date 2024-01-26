f4c4c0180d97676583589a1f184726cad841e0dd
Nicole Le <nicole.huyen.le@gmail.com>
2024-01-25 18:21:51 -0600

    finish playlist details

--- react-vite/src/components/PlaylistDetails/PlaylistDetails.jsx
+++ react-vite/src/components/PlaylistDetails/PlaylistDetails.jsx
@@ -5,27 +5,25 @@ import { loadOnePlaylistThunk} from "../../redux/playlists";
 import { loadPlaylistSongsThunk } from "../../redux/songs";
 import { loadUsersThunk } from "../../redux/users";
 import './PlaylistDetails.css'
+import { loadAlbumsThunk } from "../../redux/albums";
 
 export default function PlaylistDetails() {
     const dispatch = useDispatch()
     const { playlistId } = useParams()
     const playlist = useSelector(state => state.playlists[playlistId])
     const user = useSelector(state => state.users)
-    const playlistSongs = useSelector(state => state.playlists?.songs)
-    // const songKeys = playlistSongs.keys()
-    console.log('playlist', playlistSongs)
+    const playlistSongs = useSelector(state => state.songs.songs)
+    const album = useSelector(state => state.albums)
 
     useEffect(() => {
         dispatch(loadOnePlaylistThunk(playlistId))
         dispatch(loadUsersThunk())
         dispatch(loadPlaylistSongsThunk(playlistId))
+        dispatch(loadAlbumsThunk())
     }, [dispatch, playlistId])
 
-    // function totalSongs() {
-    //     const total = 0;
-    //     for (let i = 0; )
-    // }
-    if (!playlist) return null
+    if (!playlist || !playlistSongs) return null
+    const songKeys = Object.values(playlistSongs)
 
     return (
         <section className="playlist-details-section">
@@ -37,19 +35,42 @@ export default function PlaylistDetails() {
                     <p style={{ fontSize: 14, color: '#b3b3b3', whiteSpace: 'nowrap' }}>{playlist?.description}</p>
                     <div className="playlist-user-songs">
                         <i style={{ fontSize: 24 }} className="fa-solid fa-circle-user" />
-                        <p style={{ paddingLeft: 5, fontSize: 14 }}>{`${user[playlist?.creator_id]?.username}  •  # likes  •  # songs`}</p>
+                        <p style={{ paddingLeft: 5, fontSize: 14 }}>{`${user[playlist?.creator_id]?.username}  •  # likes  •  ${Object.keys(playlistSongs).length} songs`}</p>
                     </div>
                 </div>
             </div>
             <div className="playlist-song-list">
                 <div className="song-list-symbols">
                     <div className="playlist-play-button">
-                        <i className="fa-solid fa-play fa-2xl"></i>
+                        <i className="fa-solid fa-play fa-2xl play-icon"></i>
                     </div>
                     <i style={{ fontSize: 40 }} className="fa-regular fa-heart playlist-icon"></i>
                     <i style={{ fontSize: 30 }} className="fa-solid fa-ellipsis playlist-icon"></i>
                 </div>
-
+                <div className="song-list-info-header">
+                    <div className="hashtag-title">
+                        <p className="hashtag">#</p>
+                        <p>Title</p>
+                    </div>
+                    <p>Album</p>
+                    <i className="fa-regular fa-clock"></i>
+                </div>
+                <div className="song-info">
+                    {songKeys?.map(song => (
+                        <div key={song?.id} className="playlist-song-tile">
+                            <div className="song-info-div">
+                                <p className="song-id">{song?.id}</p>
+                                <img className='song-cover-img' src={song?.song_cover_url} alt='song-cover' />
+                                <div className="song-name-artist">
+                                    <p style={{ fontWeight: 'bold', color: 'white', whiteSpace: 'nowrap' }}>{song?.song_name}</p>
+                                    <p>{user[song?.artist_id]?.username}</p>
+                                </div>
+                            </div>
+                            <p className="song-album-name">{album[song?.album_id]?.album_name}</p>
+                            <p>duration</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     )
