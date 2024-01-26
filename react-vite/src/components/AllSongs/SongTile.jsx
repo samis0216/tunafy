a20d98bd58f5cf76977a7a31358631417a0084c6
Sami <samisamman26@yahoo.com>
2024-01-26 09:13:58 -0800

    fixing merge

--- react-vite/src/components/AllSongs/SongTile.jsx
+++ react-vite/src/components/AllSongs/SongTile.jsx
@@ -1,47 +1,33 @@
-import { useContext } from 'react'
+import { useState, useContext } from 'react'
 import { MusicContext } from "../../context/MusicContext";
 import './SongTile.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 export default function SongTile({ song, albums, artist }) {
-    const navigate = useNavigate()
     const [srv, setSrv] = useContext(MusicContext);
 
-    // const minutes = Math.floor(song.duration / 60)
-    // let seconds = song.duration % 60
-    // if (seconds < 10) seconds = `0${seconds}`
+    const minutes = Math.floor(song.duration / 60)
+    let seconds = song.duration % 60
+    if (seconds < 10) seconds = `0${seconds}`
     return (
         <>
             <div onClick={() => setSrv(`${song.song_file_url}`)}>
-                {clicked ?
-                    <div className="song-tile" id="clicked" onClick={() => setClicked(false)}>
-                        <div id='pic' onClick={() => setClicked(true)}>
-                            <p style={{ width: 10 }}>{song.id}</p>
-                            <img src={song.song_cover_url} className='album-picture-small' />
-                            <div className='song-items'>
-                                <p style={{ color: 'white' }}>{song.song_name}</p>
-                                <p>{artist?.username}</p>
-                            </div>
-                        </div>
-                        {albums[song.album_id] ? <p>{albums[song.album_id].album_name}</p> : <p>None</p>}
-                        <p>{`${minutes}:${seconds}`}</p>
-                    </div> :
-
-                    <div className="song-tile" onClick={() => setClicked(true)}>
+                    <div className="song-tile">
                         <div id='pic'>
                             <p style={{ width: 10 }}>{song.id}</p>
                             <img src={song.song_cover_url} className='album-picture-small' />
                             <div className='song-items'>
-                                <p style={{ color: 'white' }}>{song.song_name}</p>
+                                <Link to={`/songs/${song.id}`}><p style={{ color: 'white' }}>{song.song_name}</p></Link>
                                 <p>{artist?.username}</p>
                             </div>
                         </div>
                         {albums[song.album_id] ? <p>{albums[song.album_id].album_name}</p> : <p>None</p>}
                         <p>{`${minutes}:${seconds}`}</p>
-                    </div>}
-
+                    </div>
             </div>
         </>
+
+
     )
 }
