8b3da2831fa8f0b2430e7aec57205e56565dc731
Sami <samisamman26@yahoo.com>
2024-01-25 22:16:03 -0800

    fixed song-tiles

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -49,10 +49,32 @@ def oneSong(id):
     song = Song.query.get(id)
     return song.to_dict()
 
-@song_routes.route("/<int:id>/update")
+@song_routes.route("/<int:id>/update", methods=['POST'])
 def editSong(id):
     song = Song.query.get(id)
-    song.song_name
+    remove_img_from_s3(song.song_cover_url)
+    remove_song_from_s3(song.song_file_url)
+    db.session.delete(song)
+    db.session.commit()
+    form = SongForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    newDuration = math.floor(MP3(form.song_file_url.data).info.length)
+    print(form.data)
+    if form.validate_on_submit():
+        data = form.data
+        form.song_cover_url.data.filename = get_unique_filename_img(form.song_cover_url.data.filename)
+        form.song_file_url.data.filename = get_unique_filename_songs(form.song_file_url.data.filename)
+        newDuration = math.floor(MP3(form.song_file_url.data).info.length)
+        print(newDuration)
+        new_song = Song(song_name=data['song_name'],
+                        artist_id=data['artist_id'],
+                        song_cover_url=upload_img_to_s3(form.song_cover_url.data).get("url"),
+                        song_file_url=upload_song_to_s3(form.song_file_url.data).get("url"),
+                        duration=newDuration)
+        db.session.add(new_song)
+        db.session.commit()
+        return redirect('/api/songs/<int:id>')
+    return 'Bad Data'
 
 @song_routes.route("/<int:id>", methods=['DELETE'])
 def deleteSong(id):
@@ -61,4 +83,4 @@ def deleteSong(id):
     remove_song_from_s3(song["song_file_url"])
     db.session.delete(song)
     db.session.commit()
-    return "Successfully deleted"
+    return redirect('/api/songs')
--- react-vite/src/components/AllSongs/SongTile.jsx
+++ react-vite/src/components/AllSongs/SongTile.jsx
@@ -1,12 +1,12 @@
 import { useState, useContext } from 'react'
 import { MusicContext } from "../../context/MusicContext";
 import './SongTile.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 export default function SongTile({ song, albums, artist }) {
+    const navigate = useNavigate
     const [srv, setSrv] = useContext(MusicContext);
-    const [clicked, setClicked] = useState(false)
 
     const minutes = Math.floor(song.duration / 60)
     let seconds = song.duration % 60
@@ -14,33 +14,18 @@ export default function SongTile({ song, albums, artist }) {
     return (
         <>
             <div onClick={() => setSrv(`${song.song_file_url}`)}>
-                {clicked ?
-                    <div className="song-tile" id="clicked" onClick={() => setClicked(false)}>
+                    <div className="song-tile">
                         <div id='pic' onClick={() => setClicked(true)}>
                             <p style={{ width: 10 }}>{song.id}</p>
                             <img src={song.song_cover_url} className='album-picture-small' />
                             <div className='song-items'>
-                                <p style={{ color: 'white' }}>{song.song_name}</p>
+                                <Link to={`/songs/${song.id}`}><p style={{ color: 'white' }}>{song.song_name}</p></Link>
                                 <p>{artist?.username}</p>
                             </div>
                         </div>
                         {albums[song.album_id] ? <p>{albums[song.album_id].album_name}</p> : <p>None</p>}
                         <p>{`${minutes}:${seconds}`}</p>
-                    </div> :
-
-                    <div className="song-tile" onClick={() => setClicked(true)}>
-                        <div id='pic'>
-                            <p style={{ width: 10 }}>{song.id}</p>
-                            <img src={song.song_cover_url} className='album-picture-small' />
-                            <div className='song-items'>
-                                <p style={{ color: 'white' }}>{song.song_name}</p>
-                                <p>{artist?.username}</p>
-                            </div>
-                        </div>
-                        {albums[song.album_id] ? <p>{albums[song.album_id].album_name}</p> : <p>None</p>}
-                        <p>{`${minutes}:${seconds}`}</p>
-                    </div>}
-
+                    </div>
             </div>
         </>
 
