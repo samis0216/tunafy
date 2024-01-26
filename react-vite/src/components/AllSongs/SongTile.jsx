529fa65eec2d7e2f80c66ad67edb29be6b978b2b
Sami <samisamman26@yahoo.com>
2024-01-25 16:58:57 -0800

    fixed lyndons mistakes

--- react-vite/src/components/AllSongs/SongTile.jsx
+++ react-vite/src/components/AllSongs/SongTile.jsx
@@ -4,7 +4,7 @@ import './SongTile.css'
 import { Link } from 'react-router-dom'
 
 
-export default function SongTile({ song, albums, artist}) {
+export default function SongTile({ song, albums, artist }) {
     const [srv, setSrv] = useContext(MusicContext);
     const [clicked, setClicked] = useState(false)
 
@@ -12,49 +12,38 @@ export default function SongTile({ song, albums, artist}) {
     let seconds = song.duration % 60
     if (seconds < 10) seconds = `0${seconds}`
     return (
-
-        <div className="song-tile">
-            <div id='pic'>
-                <p style={{ width: 10 }}>{song.id}</p>
-                <img src={song.song_cover_url} className='album-picture-small' />
-                <div className='song-items'>
-                    <div className={'hover-song'}><Link style={{ color: 'white', textDecoration: 'none' }} to={`/songs/${song.id}`}>{song.song_name}</Link></div>
-                    <p>{artist?.username}</p>
-                </div>
-
-        <div onClick={()=> setSrv(`${song.song_file_url}`)}>
-            {clicked ?
-                <div className="song-tile" id="clicked" onClick={()=> setClicked(false)}>
-                    <div id='pic' onClick={() => setClicked(true)}>
-                        <p style={{ width: 10 }}>{song.id}</p>
-                        <img src={song.song_cover_url} className='album-picture-small' />
-                        <div className='song-items'>
-                            <p style={{ color: 'white' }}>{song.song_name}</p>
-                            <p>{artist?.username}</p>
+        <>
+            <div onClick={() => setSrv(`${song.song_file_url}`)}>
+                {clicked ?
+                    <div className="song-tile" id="clicked" onClick={() => setClicked(false)}>
+                        <div id='pic' onClick={() => setClicked(true)}>
+                            <p style={{ width: 10 }}>{song.id}</p>
+                            <img src={song.song_cover_url} className='album-picture-small' />
+                            <div className='song-items'>
+                                <p style={{ color: 'white' }}>{song.song_name}</p>
+                                <p>{artist?.username}</p>
+                            </div>
                         </div>
-                    </div>
-                    {albums[song.album_id] ? <p>{albums[song.album_id].album_name}</p> : <p>None</p>}
-                    <p>{`${minutes}:${seconds}`}</p>
-                </div> :
+                        {albums[song.album_id] ? <p>{albums[song.album_id].album_name}</p> : <p>None</p>}
+                        <p>{`${minutes}:${seconds}`}</p>
+                    </div> :
 
-                <div className="song-tile" onClick={() => setClicked(true)}>
-                    <div id='pic'>
-                        <p style={{ width: 10 }}>{song.id}</p>
-                        <img src={song.song_cover_url} className='album-picture-small' />
-                        <div className='song-items'>
-                            <p style={{ color: 'white' }}>{song.song_name}</p>
-                            <p>{artist?.username}</p>
+                    <div className="song-tile" onClick={() => setClicked(true)}>
+                        <div id='pic'>
+                            <p style={{ width: 10 }}>{song.id}</p>
+                            <img src={song.song_cover_url} className='album-picture-small' />
+                            <div className='song-items'>
+                                <p style={{ color: 'white' }}>{song.song_name}</p>
+                                <p>{artist?.username}</p>
+                            </div>
                         </div>
-                    </div>
-                    {albums[song.album_id] ? <p>{albums[song.album_id].album_name}</p> : <p>None</p>}
-                    <p>{`${minutes}:${seconds}`}</p>
-                </div>}
+                        {albums[song.album_id] ? <p>{albums[song.album_id].album_name}</p> : <p>None</p>}
+                        <p>{`${minutes}:${seconds}`}</p>
+                    </div>}
 
             </div>
-            {albums[song.album_id] ? <p>{albums[song.album_id].album_name}</p> : <p>None</p>}
-            <p>{`${minutes}:${seconds}`}</p>
-        </div>
-    </div>
+        </>
+
 
     )
 }
