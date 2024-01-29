79285a41ad58402381c649e7e1a99fb2fb40fa45
Sami <samisamman26@yahoo.com>
2024-01-29 03:29:12 -0800

    cool

--- migrations/versions/20240129_032628_.py
+++ migrations/versions/20240129_032628_.py
@@ -1,13 +1,15 @@
 """empty message
 
 Revision ID: 763f10f93355
-Revises: 
+Revises:
 Create Date: 2024-01-29 03:26:28.338420
 
 """
 from alembic import op
 import sqlalchemy as sa
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '763f10f93355'
@@ -75,6 +77,13 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE albums SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE playlists SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE songs SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE playlist_songs SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE song_likes SET SCHEMA {SCHEMA};")
 
 
 def downgrade():
