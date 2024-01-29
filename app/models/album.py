33f7018226471a0e226d367608a91a605b5be3d1
Sami <samisamman26@yahoo.com>
2024-01-29 03:17:41 -0800

    what

--- app/models/album.py
+++ app/models/album.py
@@ -7,7 +7,7 @@ class Album(db.Model):
       __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    album_name = db.Column(db.String(30), nullable=False)
+    album_name = db.Column(db.String(45), nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     album_cover_url = db.Column(db.String, nullable=False)
 
