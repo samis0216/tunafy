00a66676a5fb7274b5cf58e5f03dbc6ff975ac93
Sami <samisamman26@yahoo.com>
2024-03-01 13:24:02 -0500

    final

--- app/__init__.py
+++ app/__init__.py
@@ -39,7 +39,6 @@ app.register_blueprint(collection_routes, url_prefix='/api/collection')
 
 db.init_app(app)
 
-#! DELETE BEFORE ANYTHING IMPORTANT
 if 'sqlite' in app.config['SQLALCHEMY_DATABASE_URI']:
     def _fk_pragma_on_connect(dbapi_con, con_record):  # noqa
         dbapi_con.execute('pragma foreign_keys=ON')
@@ -47,7 +46,6 @@ if 'sqlite' in app.config['SQLALCHEMY_DATABASE_URI']:
     with app.app_context():
         from sqlalchemy import event
         event.listen(db.engine, 'connect', _fk_pragma_on_connect)
-#! DELETE BEFORE ANYTHING IMPORTANT
 
 Migrate(app, db)
 
